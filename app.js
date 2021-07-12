429beb06de4d7863d02d72c5948740873adeb37a
Annie Huang <annie.hn@gmail.com>
2021-07-12 21:45:57 +1000

    Add jumping logic

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelector('.carousel-slide img');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
 
 // Buttons
 const prevBtn = document.querySelector('#prevBtn');
@@ -7,9 +7,7 @@ const nextBtn = document.querySelector('#nextBtn');
 
 // Counter
 let counter = 1;
-// const size = carouselImages[0].clientWidth;
-// const size = carouselImages[0].getBoundingClientRect().width;
-const size = carouselImages[0].offsetWidth;
+const size = carouselImages[0].clientWidth;
 console.log('size=', size);
 
 carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
@@ -32,5 +30,19 @@ prevBtn.addEventListener('click', () => {
 });
 
 carouselSlide.addEventListener('transitionend', () => {
-  console.log('Fired');
+  // console.log('Fired');
+  console.log(carouselImages[counter]);
+
+  // If transition result we landing on the first element, img/1.jfif (2nd one in our list), don't do transition animation, jump directly to the last element, img/6.jfif  (the 2nd last element)
+  if (carouselImages[counter].id === 'lastClone') {
+    carouselSlide.style.transition = 'none'; // if you comment out this line, you can see the ugly jump
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+  }
+
+  if (carouselImages[counter].id === 'firstClone') {
+    carouselSlide.style.transition = 'none'; // if you comment out this line, you can see the ugly jump
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+  }
 });
